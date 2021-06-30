1c1c5f8eecc91b4cfcb3691437e8c66757769c5c
mansur-khan-cs <74323218+mansur-khan-cs@users.noreply.github.com>
2021-06-30 14:04:57 +0200

    Fix: Resolve issue with "?" or "!" or "." ending in command line

--- src/main/mainapp.cpp
+++ src/main/mainapp.cpp
@@ -7,6 +7,10 @@ using namespace std;
 int main(int argc, char *argv[]){
 	vector<string> in;
 	for(int i=1; i<argc; i++){
+		if(i==argc-1) {
+			string s = argv[i];
+			in.push_back(s.substr(0, s.size()-1));
+		}
 		in.push_back(argv[i]); 
 	}
 	Intent intent(in);
