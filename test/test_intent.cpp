a657aea1077611503af4e395489d3945347a2b72
mansur-khan-cs <mansur.khan.cs@gmail.com>
2021-06-30 14:00:17 +0200

    Test: Add multiple test sections

--- test/test_intent.cpp
+++ test/test_intent.cpp
@@ -2,12 +2,32 @@
 #include "catch.h"
 #include "Intent.h"
 
-TEST_CASE( "RecognizeIntent") {      
+TEST_CASE( "RecognizeIntent") {
     Intent intent;  
-    
+
     SECTION("Should get weather intent"){
         std::vector<std::string> v = {"What", "is", "the", "weather", "like", "today?"};
         intent.setIntent(v);
         REQUIRE( intent.recognize() ==  "Intent: Get Weather");
     }
+    SECTION("Should get weather intent in city(Paris)"){
+        std::vector<std::string> v = {"What", "is", "the", "weather", "like", "in", "Paris", "today?"};
+        intent.setIntent(v);
+        REQUIRE( intent.recognize() ==  "Intent: Get Weather City");
+    }
+    SECTION("Should get weather intent in city(Frankfurt)"){
+        std::vector<std::string> v = {"What", "is", "the", "weather", "like", "in", "Frankfurt", "today?"};
+        intent.setIntent(v);
+        REQUIRE( intent.recognize() ==  "Intent: Get Weather City");
+    }
+    SECTION("Should get weather intent in city(Berlin)"){
+        std::vector<std::string> v = {"What", "is", "the", "weather", "like", "in", "Berlin", "today?"};
+        intent.setIntent(v);
+        REQUIRE( intent.recognize() ==  "Intent: Get Weather City");
+    }      
+    SECTION("Should get fact intent"){
+        std::vector<std::string> v = {"Tell", "me", "an", "interesting", "fact"};
+        intent.setIntent(v);
+        REQUIRE( intent.recognize() ==  "Intent: Get Fact");
+    }        
 }
